0f3f8a8a3e9969cac729c443fa15e94388e2212f
robzgf <rgrielfilho@gmail.com>
2019-09-23 21:08:58 -0300

    Anotações e Exemplos - Criar e Apagar DB

---
+++ anotações.sql
@@ -0,0 +1,6 @@
+#Base de Dados é todo e qualquer lugar que armazena informação.
+#Banco de Dados é onde gerencia e manipula os dados
+#SQL significa Linguagem de Pesquisa Estruturada (Search Query Language)
+#MySQL foi criado em 1996 pela TSX que foi comprada pela Sun e que foi comprada no final pela Oracle
+
+#Para apagar um banco de dados, ele não pode estar em uso. 
---
+++ querys.sql
@@ -0,0 +1,16 @@
+#Criar um banco de dados. Não utilizar acentuação ou caracteres especiais. 
+#CREATE DATABASE nome_do_banco;
+CREATE DATABASE db_curso_mysql;
+CREATE DATABASE db_testes;
+
+#Entrar (usar) o banco de dados
+#USE nome_do_banco;
+USE db_curso_mysql;
+USE db_testes;
+
+#Para apagar um banco de dados, primeiro saia dele
+#USE outro_banco;
+#DROP DATABASE nome_banco;
+DROP DATABASE db_testes;
+
+
